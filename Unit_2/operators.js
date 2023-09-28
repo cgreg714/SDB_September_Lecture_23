14d871639ffebe832b422e7ac0a811e7d2d35366
cgreg714 <c.gregoire714@gmail.com>
2023-09-27 20:42:46 -0400

    Finished Operator Lesson

---
+++ Unit_2/operators.js
@@ -0,0 +1,114 @@
+/*! Operators
+    These are what help us manipulate our data so that we can update, increase, separate, or simply change it all together.
+    - Plus        +
+    - Times       *
+    - Power       **
+    - Dot         .
+    - Assignment  =
+    - Comparison  ==
+    
+    Used to:
+        - Assign
+        - Compare
+        - Arithmetic
+        - Logical
+        - Conditional
+
+    Expressions & Statement
+
+    ex.
+    keyword name (expression) {
+        code block that runs
+    }
+
+*/
+
+/*
+    Comparison Operators
+    - JS is helpful and does something called "coercion" when comparing values 
+        -Coercion is the process of converting a value from one type to another.
+
+*/
+
+// Equal To, ==
+// JS assumed we wanted to check if 3 equaled 3. This is how to check values.
+
+console.log("3" == 3); //True, only checking values
+
+// Strict Equal To, ===
+// Checks if the two values AND the two datatypes are equal.
+// Equal value and of the  same datatype.
+
+console.log("3" === 3); // false, because it is checking both the value AND the datatype
+console.log(3 == 3); // true
+
+// Not Equal To, != (only checking values)
+
+console.log("3" != 3); // false
+console.log("3" != 4); // true
+
+// Not Equal To, !== (checking both datatype AND values)
+
+console.log(3 !== 3); // false because 3 and 3 are equal
+console.log("3" !== 4); // true, because they are different datatypes
+
+// Greater Than
+3 > 2; //true
+
+// Less Than
+2 < 3; //true
+
+// Greater Than or Equal To, >=
+// ! 'The Greater Than' operator must be used BEFORE the equal sign in order to execute correctly
+
+console.log( 18 >= 3 );// true
+console.log( 7 >= 7 );//true
+console.log(4 >= 7);// false
+
+// Less Than or Equal To, <=
+// ! The 'Less Than' operator must be used BEFORE the equal sign.
+
+console.log( 18 <= 3 );// false
+console.log( 7 <= 7 );//true
+console.log(4 <= 7);// false
+
+// And, &&
+2 && 3;
+
+// And/Or, || (check if this number is equal to this amount or that amount.)
+2 || 3;
+
+/* 
+    Logical Operators
+    - Arithmatic operators
+*/
+
+//Addition Operator, +=
+let r=4;
+console.log(r += 1); // r = r + 1 answer is 5
+console.log(r); // 5
+
+//Subtraction Operator, -=
+let n=4
+console.log(n -= 3); // 1
+console.log(n); // 1
+
+//Multiplication Operator, *=
+let m=2;
+console.log(m *= 2); // 4
+console.log(m); // 4
+
+//Division Operator, /=
+let q=4;
+console.log(q /= 2); // 2
+console.log(q); // 2
+
+//Remainder Operator / Modulus Operator, %=
+let t=7;
+console.log(t %= 2); // 1
+console.log(t); // 1
+
+//Exponential Operator, **=
+let y=2;
+console.log(y **= 2); //4  = 2 to the second power
+console.log(y); //4
